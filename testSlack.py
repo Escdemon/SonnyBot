8349c2e395931bdb7d019d7c8a0597ac895c63a8
NEISSEN <paul.neissen@cgi.com>
2018-03-27 17:27:49 +0200

    rtm_connect

--- testSlack.py
+++ testSlack.py
@@ -4,50 +4,76 @@ import re
 import json
 from slackclient import SlackClient
 
-TOKEN = open("E:/Documents/Bots/SonnyBot/token.txt", "r").read()
+PATH = "C:/Users/paul.neissen/Documents/Bots/SonnyBot"
+TOKEN = open(PATH + "/token.txt", "r").read()
 
 sc = SlackClient(TOKEN)
 
+#Target channel #testsonny #paul.neissen #alice.montel
+target = "paul.neissen" 
+
+#User list
 userList = sc.api_call("users.list")
-
-target = "paul.neissen"
-
 for i in userList['members']:
-    #print(i['name'] + " " + i['id'])
+    print(i['name'] + " " + i['id'])
     if i['name'] == target:
         target = i['id']
+        
+# Channel list
+channelList = sc.api_call(
+  "channels.list",
+  exclude_archived=1
+)
+for i in channelList['channels']:
+    print(i['name'] + " : " + i['id'])
+
+groupList = sc.api_call(
+  "groups.list",
+  exclude_archived=1
+)
+for i in groupList['groups']:
+    print(i['name'] + " : " + i['id'])
+    if i['name'] == target:
+        target = i['id']
+
+
         
 def sendMessagetest():
     sc.api_call(
       "chat.postMessage",
       channel=target,
-      text="Hello from Python! :tada:",
-      #username = "Sonny",
-      #icon_url = "https://d3evv04q39b0w5.cloudfront.net/prod/app/pages/home/images/chatbot-person.png",
+      text="Hi :)",
       as_user = "true"
     )
 
-with open("E:/Documents/Bots/SonnyBot/data.json") as json_file:
+def treatEvent(e):
+    a=  0
+    
+
+with open(PATH + "/data.json") as json_file:
     json_decoded = json.load(json_file)
 
 json_decoded["Bonjour"] = "END_SENTENCE"
 
-with open("E:/Documents/Bots/SonnyBot/data.json", 'w') as json_file:
+with open(PATH + "/data.json", 'w') as json_file:
     json.dump(json_decoded, json_file)
 
 
-print(data)
 #sendMessagetest()
 
-'''
-if sc.rtm_connect():
-    print("Connection Succeed")
-    while True:
-        e = sc.rtm_read()
-        if len(e):
-            print(e)
-        time.sleep(1)
-    print("End")
-else:
-    print("Connection Failed")
-'''
+
+while True:
+    connect = sc.rtm_connect(auto_reconnect=True)
+    print(connect)
+    if connect:
+        print("Connection Succeed")
+        while True:
+            e = sc.rtm_read()
+            if len(e):
+                print(e)
+                treatEvent(e)
+                
+            time.sleep(1)
+        print("End")
+    else:
+        print("Connection Failed")
