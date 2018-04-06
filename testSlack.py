72e299c3ae55fffee5ec6040918b3ff521f7844e
NEISSEN <paulneissen@gmail.com>
2018-04-06 16:35:49 +0200

    music

--- testSlack.py
+++ testSlack.py
@@ -14,6 +14,7 @@ DATA_FILE = "data.json"
 CUTTIE_DATA_FILE = "cuttie-data.json"
 CUTTIE_ID = "<@U7UG5HLLV>"
 CUTTIE = "U7UG5HLLV"
+MUSIC_CHAN = 'G9YB0SM96'
 
 sc = SlackClient(TOKEN)
 
@@ -74,31 +75,32 @@ def saveCuttieData(t):
 
 def treatEvent(e):
     for event in e:
-        if event['type'] == "message" and event['user'] != 'U87N79D25':
-            t = event['text'].split()
-            for i,e in enumerate(t):
-                if e.lower() in NICK_LIST:
-                    t[i] = NICK
-            #TODO : remove forbidden word in t
-            if t[0] not in jsonData[START]:
-                jsonData[START][t[0]] = 1
-            else:
-                jsonData[START][t[0]] += 1
-            for i,word in enumerate(t):
-                nextword = t[i+1] if i+1 < len(t) else END
-                if word not in jsonData:
-                    jsonData[word] = {}
-                    jsonData[word][nextword] = 0
-                if nextword not in jsonData[word] :
-                    jsonData[word][nextword] = 1
+        if 'text' in event:
+            if event['type'] == "message" and event['user'] != 'U87N79D25'  and event['channel'] != MUSIC_CHAN:
+                t = event['text'].split()
+                for i,e in enumerate(t):
+                    if e.lower() in NICK_LIST:
+                        t[i] = NICK
+                #TODO : remove forbidden word in t
+                if t[0] not in jsonData[START]:
+                    jsonData[START][t[0]] = 1
                 else:
-                    jsonData[word][nextword] += 1
-            print(jsonData)
-            with open(DATA_FILE, 'w') as jsonFile:
-               json.dump(jsonData, jsonFile)
-               
-            if any(x == CUTTIE_ID for x in t):
-                saveCuttieData(t)
+                    jsonData[START][t[0]] += 1
+                for i,word in enumerate(t):
+                    nextword = t[i+1] if i+1 < len(t) else END
+                    if word not in jsonData:
+                        jsonData[word] = {}
+                        jsonData[word][nextword] = 0
+                    if nextword not in jsonData[word] :
+                        jsonData[word][nextword] = 1
+                    else:
+                        jsonData[word][nextword] += 1
+                print(jsonData)
+                with open(DATA_FILE, 'w') as jsonFile:
+                   json.dump(jsonData, jsonFile)
+                   
+                if any(x == CUTTIE_ID for x in t):
+                    saveCuttieData(t)
                 
 def sentenceToSay(data):
     r = []
@@ -124,19 +126,71 @@ def sendResponse(e):
     # peut être juste prendre le premier event ?
     for event in e:
         if event['type'] == "message":
-            t = event['text'].split()
-            text = event['text'].lower().split()
-            if any(word in text for word in NICK_LIST):
-                text = ["Bob" if any(x == word for word in NICK_LIST) else x for x in text]
-                #print(text)
-                
-                sentence = sentenceToSay(jsonCuttieData if event['user'] == CUTTIE else jsonData).replace(NICK, users[event['user']])
-                #print(sentence)
-                sendMessage(sentence, event['channel'])
+            if 'text' in event:
+                t = event['text'].split()
+                text = event['text'].lower().split()
+                if any(word in text for word in NICK_LIST):
+                    text = ["Bob" if any(x == word for word in NICK_LIST) else x for x in text]
+                    #print(text)
+                    
+                    sentence = sentenceToSay(jsonCuttieData if event['user'] == CUTTIE else jsonData).replace(NICK, users[event['user']])
+                    #print(sentence)
+                    sendMessage(sentence, event['channel'])
                 
 
+def music(e):
+    for event in e:
+        if 'text' in event:
+            if event['type'] == "message" and event['channel'] == MUSIC_CHAN:
+                
+                t = event['text'].split()
+                text = event['text'].lower().split()
+                if any(word in text for word in NICK_LIST) and any(word == 'music' for word in text):
+                    #open file
+                    d = {}
+                    with open('music.txt') as f:
+                        content = f.readlines()
+                        for line in content:
+                            tmp = line.split()
+                            if tmp[0] not in d:
+                                d[tmp[0]] = [tmp[1]]
+                            else:
+                                d[tmp[0]] += [tmp[1]]
+                    print(d)
+                    
+                    if any(word == 'add' for word in text):
+                        t = [x for x in t if x not in ['music','Music','MUSIC','metal','Metal','METAL','Dub','dub','DUB',
+                                                       'reggae','Reggae','REGGAE','<@U87N79D25>','Sonny','sonny','SONNY',
+                                                       'add','Add','ADD','other','Other']]
+                        with open('music.txt', 'a') as file:
+                            
+                            if any(word == 'metal' for word in text):
+                                file.write('METAL ' + ''.join(t) + '\n')
+                            elif any(word == 'dub' for word in text):
+                                file.write('DUB ' + ''.join(t) + '\n')
+                            elif any(word == 'reggae' for word in text):
+                                file.write('REGGAE ' + ''.join(t) + '\n')
+                            elif any(word == 'other' for word in text):
+                                file.write('OTHER ' + ''.join(t) + '\n')
+                        return True
+                    
+                    if any(word == 'metal' for word in text) and 'METAL' in d:
+                        sendMessage(' '.join(d['METAL']), event['channel'])
+                        return True
+                    if any(word == 'other' for word in text) and 'OTHER' in d:
+                        sendMessage(' '.join(d['OTHER']), event['channel'])
+                        return True
+                    if any(word == 'dub' for word in text) and 'DUB' in d:
+                        sendMessage(' '.join(d['DUB']), event['channel'])
+                        return True
+                    if any(word == 'reggae' for word in text) and 'REAGGAE' in d:
+                        sendMessage(' '.join(d['REGGAE']), event['channel'])
+                        return True
+                
+    return False
+
 while True:
-    try:
+    #try:
         connect = sc.rtm_connect(auto_reconnect=True)
         #print(connect)
         if connect:
@@ -145,14 +199,17 @@ while True:
                 e = sc.rtm_read()
                 if len(e):
                     print(e)
-                    treatEvent(e)
-                    sendResponse(e)
+                    if not(music(e)):
+                        treatEvent(e)
+                        sendResponse(e)
                 time.sleep(1)
             print("End")
         else:
             print("Connection Failed")
+            '''
     except KeyboardInterrupt:
         sys.exit()
     except:
-        continue
+        sys.exit()
+        continue'''
 
